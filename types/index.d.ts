fe755aa5e12c7c574899c5640336e2d9b2541cbf
IRSAGE <sibo.egide98@gmail.com>
2024-11-12 18:18:59 +0200

    Setting Up Dynamic File Routes

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,11 @@
-export interface UploadFileProps {
+declare interface UploadFileProps {
   file: File;
   ownerId: string;
   accountId: string;
   path: string;
+}
+
+declare interface SearchParamProps {
+  params?: Promise<SegmentParams>;
+  searchParams?: Promise<{ [key: string]: string | string[] | undefined }>;
 }
