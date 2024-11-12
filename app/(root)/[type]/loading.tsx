a5ca127a1dc7ea640708b20dcc1ee96176a4df26
IRSAGE <sibo.egide98@gmail.com>
2024-11-12 22:42:02 +0200

    Adding a loading component

---
+++ app/(root)/[type]/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function loading() {
+  return (
+    <div className="page-container flex-center h-full">
+      <p className="empty-list">Fetching....</p>
+    </div>
+  );
+}
+
+export default loading;
