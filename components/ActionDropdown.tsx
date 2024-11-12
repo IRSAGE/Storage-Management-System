ffbbdfaf3519ec05cf70e088a9d9fd3166e78c9a
IRSAGE <sibo.egide98@gmail.com>
2024-11-12 20:02:54 +0200

    Showing File Details

--- components/ActionDropdown.tsx
+++ components/ActionDropdown.tsx
@@ -26,6 +26,7 @@ import { Button } from "@/components/ui/button";
 
 import { usePathname } from "next/navigation";
 import { renameFile } from "@/lib/actions/file.actions";
+import { FileDetails } from "./ActionsmodalContent";
 
 const ActionDropdown = ({ file }: { file: Models.Document }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -78,6 +79,7 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
               onChange={(e) => setName(e.target.value)}
             />
           )}
+          {value === "details" && <FileDetails file={file} />}
         </DialogHeader>
         {["rename", "delete", "share"].includes(value) && (
           <DialogFooter className="flex flex-col gap-3 md:flex-row">
