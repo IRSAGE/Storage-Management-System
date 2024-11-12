2840273edf608bd924cbbeda8dadab754bf6c3de
IRSAGE <sibo.egide98@gmail.com>
2024-11-12 20:32:15 +0200

    Fixing Create Account

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -76,7 +76,8 @@ export const createAccount = async ({
       {
         fullName,
         email,
-        avatar: { avatarPlaceHolderUrl },
+        avatar:
+          "https://cdn2.vectorstock.com/i/1000x1000/44/01/default-avatar-photo-placeholder-icon-grey-vector-38594401.jpg",
         accountId,
       }
     );
